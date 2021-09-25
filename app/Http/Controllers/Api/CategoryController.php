f73fce58721a68d70d744df84322b1187233b68f
CodingImproveLife <web-coder@tuta.io>
2021-09-25 21:25:11 +0200

    Added categories

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return AnonymousResourceCollection
+     */
+    public function index()
+    {
+        return CategoryResource::collection(Category::all());
+    }
+}
